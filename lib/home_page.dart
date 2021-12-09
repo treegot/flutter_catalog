6ff2d89eac86d84837027fde2d5d2114fdba5666
sandip jaiswal <sandipjayaswal@gmail.com>
2021-12-09 16:12:05 +0530

    Day 2 is completed

---
+++ lib/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+void main()
+{
+  runApp(HomePage());
+
+}
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catelog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("hi i am sandip jayaswal"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_page.dart';
 
 void main()
 {
@@ -12,13 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
- home: Material(
-   child: Center(
-     child: Container(
-       child: Text("hi i am sandip jayaswal"),
-     ),
-   ),
- ),
+      home: HomePage(),
     );
   }
 }
